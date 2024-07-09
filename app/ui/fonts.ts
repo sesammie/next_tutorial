044896e31f1633564e685763ae9141cc7a8737dc
sam <s.michielsen@remoticom.com>
2024-07-09 11:04:43 +0200

    msg

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google';
-import { Roboto } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const roboto = Roboto({ weight: ['400'], subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400'], subsets: ['latin'] });
